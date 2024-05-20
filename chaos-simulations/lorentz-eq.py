14418f7d7966819b797277db5d01032ec0936e20
Williame33445 <williame15254@gmail.com>
2024-05-20 14:03:34 +0100

    added RK4 methods

---
+++ chaos-simulations/lorentz-eq.py
@@ -0,0 +1,7 @@
+import sys
+import os
+
+
+sys.path.append(os.path.abspath("."))
+
+from useful-code.RK4-methods import *
---
+++ useful-code/RK4-methods.py
@@ -0,0 +1,13 @@
+#implements rk4 methods
+#works for both vectors and scalars
+
+def iterate(t_n,y_n,h,f):
+    k_1 = f(t_n, y_n)
+    k_2 = f(t_n + h/2, y_n + h*k_1/2)
+    k_3 = f(t_n + h/2, y_n + h*k_2/2)
+    k_4 = f(t_n + h, y_n + h*k_3)
+
+    return y_n + h*(k_1 + 2*k_2 + 2*k_3 + k_4)/6
+
+
+
