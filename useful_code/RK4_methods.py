c8fc88016c442f521897def8723dd2010ef89106
Williame33445 <williame15254@gmail.com>
2024-05-26 13:59:58 +0100

    added some general methods to rk4.py

--- useful_code/RK4_methods.py
+++ useful_code/RK4_methods.py
@@ -1,7 +1,9 @@
-#implements rk4 methods
-#works for arrays 
+
 
 def iterateRK4(t_n,y_n,h,f):
+    """
+    Iterates by RK4 method.
+    """
     k_1 = f(t_n, y_n)
     k_2 = f(t_n + h/2, y_n + h*k_1/2)
     k_3 = f(t_n + h/2, y_n + h*k_2/2)
@@ -10,4 +12,21 @@ def iterateRK4(t_n,y_n,h,f):
     return y_n + h*(k_1 + 2*k_2 + 2*k_3 + k_4)/6
 
 
+def simulateDuffing(t_0,y_0,numberOfRuns,h,f):
+    """
+    Uses RK4 method to find a set of points on the solution to dY/dt=f(t,Y) given an intial condition.
+    """
+    yList = [y_0]
+    tList = [t_0]
+
+    for x in range(numberOfRuns):
+        yList.append(iterateRK4(tList[-1],yList[-1],f))
+        tList.append(tList[-1]+h)
+    return tList, yList
+
+def transformToXYZ(lst,N):
+    """
+    Transfroms [np.array([1,2]),np.array([3,4])] to [[1,3],[2,4]]
+    """
+    return [[s[c] for s in lst] for c in range(N)]
 
