a9c79a9b72e89f307aaf8ab8c90c8e12f115c126
Williame33445 <williame15254@gmail.com>
2024-05-28 15:29:27 +0100

    added numerov method

--- useful_code/numerov_method.py
+++ useful_code/numerov_method.py
@@ -1,3 +1,15 @@
 # method solves d^2X(t)/dx^2 = f(t)X(t) for boundary conditions X(t_1) = X_1, X(t_2) = X_2
 # to h^6
 
+def w(x,t,h,f):
+    return (1 - f(t)*(h**2)/12)*x
+
+def wInv(w,t,h,f):
+    return w/(1 - f(t)*(h**2)/12)
+
+def iterateNumerov(x_t,x_tMinush,t,h,f):
+    w_t = w(x_t,t,h,f)
+    w_tMinush = w(x_tMinush,t,h,f)
+    w_rPlush = 2*w_t - w_tMinush + x_t*f(t)*(h**2)
+    return wInv(w_rPlush,t,h,f)
+
