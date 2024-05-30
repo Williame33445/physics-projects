had to fix some silly errors, program now gives answers out by around a factor of 2, testing is no longer up to date

quantum-scattering/hydrogen-scattering-by-krypton-atoms.py
@@ -1,12 +1,35 @@
 import sys
 import os
 from scipy import constants
+import numpy as np
 from spherically_symetric_quantum_scattering import ScatteringSystem
 
-# some constants
-epsilon = 5.9*(10**-3)*constants.elementary_charge
-rho = 3.67*constants.angstrom
+# some constants: energy is measure in meV and distance in units of rho
+# means 2m/hbar^2 = 6.12 meV^-1 rho^-2
+epsilon = 5.9
+rho = 1
+alpha = 6.12
+C = np.sqrt(epsilon*alpha/25)
 
 def V_LJ(r):
     return epsilon*((rho/r)**12 - 2*((rho/r)**6))
 
+def uSmallr(r):
+    return np.exp(-C/(r**5))
+
+
+#find intial conditions and h
+r_0 = 0.5*rho
+r_1 = 0.8*rho
+h = r_1 - r_0
+u_0 = uSmallr(r_0)
+u_1 = uSmallr(r_1)
+
+#system parameters
+E = 1
+r_end = 10000
+
+#run simulation
+scatteringSys = ScatteringSystem(E,V_LJ,r_0,h,u_0,u_1,r_end,6,simpleUnits=False)
+
+print(scatteringSys.totalCrossSection)

quantum-scattering/spherically_symetric_quantum_scattering.py
@@ -2,40 +2,83 @@ import sys
 import os
 import numpy as np
 from scipy import constants
+from scipy import special
 
 sys.path.append(os.path.abspath("."))
 from useful_code.numerov_method import *
 
+def findPhaseShift(r_max,u_max,r_end,u_end,l):
+    K = (r_max*u_end)/(r_end*u_max)
+    numerator = K*special.spherical_jn(l,r_max) - special.spherical_jn(l,r_end)
+    denominator = K*special.spherical_yn(l,r_max) - special.spherical_yn(l,r_end)
+    return np.arctan(numerator/denominator)
+
+
 class ScatteringSystem:
-    def __init__(self,E,l,m,V,r_0,h,u_0,u_1,r_end,simpleUnits=False):
+    def __init__(self,E,V,r_0,h,u_0,u_1,r_max,lCutoff,simpleUnits=False):
         #system parameters
         self.E = E
-        self.l = l
-        self.m = m
+        self.k = np.sqrt(6.12*E)
+        self.lambbda = 2*np.pi/self.k
         self.V = V
         self.simpleUnits = simpleUnits
 
+        #cutoff for sums in l
+        self.lCutOff = lCutoff
+
         #simulation parameters
-        self.r_0 = r_0
         self.h = h
-        self.N = int(np.ceil((r_end-r_0)/h))
+        self.N = int(np.ceil((r_max + self.lambbda - r_0)/h))
 
-        self.rList,self.uList = runNumerov(r_0,h,u_0,u_1,self.F,self.N)
+        #intial and final r values
+        self.r_0 = r_0
+        self.r_maxIndex = int(np.ceil((r_max-r_0)/h))
+        self.r_max = h*self.r_maxIndex + r_0
+        self.r_end = r_0 + self.N*h
 
-    def F(self,r):
+        self.uLists = []
+        for l in range(self.lCutOff):
+            F = lambda r: self.FL(r,l)
+            self.rList, ulList = runNumerov(r_0,h,u_0,u_1,F,self.N)
+            self.uLists.append(ulList)
+
+        self.findPhaseShifts()
+        self.findTotalCrossSection()
+
+    def FL(self,r,l):
         #removes certain divergences that don't actually occur
         if r == 0 and self.r_0 == 0:
             return 0
         
         if self.simpleUnits:
-            return self.V(r) + self.l*(self.l+1)/(r**2) - self.E
+            return self.V(r) + l*(l+1)/(r**2) - self.E
         
-        return 2*self.m*self.V(r)/(constants.hbar**2) + self.l*(self.l+1)/(r**2) - 2*self.m*self.E/(constants.hbar**2)
+        #choose units of p^2 for E and V is 
+        return 6.12*self.V(r) + l*(l+1)/(r**2) - 6.12*self.E
+
+    def findPhaseShifts(self):
+        self.phaseShifts = []
+        for l in range(self.lCutOff):
+            u_max = self.uLists[l][self.r_maxIndex]
+            u_end = self.uLists[l][-1]
+            self.phaseShifts.append(findPhaseShift(self.r_max,u_max,
+                                           self.r_end,u_end,l))
+            
+    def findTotalCrossSection(self):
+        elementsOfSum = [(2*l + 1)*(np.sin(deltaL)**2) for l,deltaL in enumerate(self.phaseShifts)]
+        self.totalCrossSection = 4*np.pi*sum(elementsOfSum)/(self.k**2)
 
 
-    def findNearestu(self,r):
-        index = np.floor(r/self.h)
-        return self.uList[index]
+
+
+
+
+
+
+
+
+
+        
 
         
         