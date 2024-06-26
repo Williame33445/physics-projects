194e51b768ba09bb7b435879cb00c89cce4ea5f6
Williame33445 <williame15254@gmail.com>
2024-06-26 17:50:36 +0100

    tidied up code, still has problems

--- electronic_structure/density-functional-theory/helium-atom.py
+++ electronic_structure/density-functional-theory/helium-atom.py
@@ -3,13 +3,9 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 sys.path.append(os.path.abspath("."))
 from useful_code.numerov_method import *
 from useful_code.verlet_method import *
-"""
-Should remove function .argmin() part and just work with index form
-"""
 
 def fKS(E,hatreePot):
     """
@@ -21,12 +17,9 @@ def uAsym(r):
     """
     Finds asymptotic solution
     """
-    return np.sqrt(32)*r*np.exp(-2*r) #need to add correct normalisation
+    return np.sqrt(32)*r*np.exp(-2*r)
 
-#is this the correct solution, does it need to be normalised?
-#factor here doesn't seem to make a difference to intial conditions
-#also coverges very quickly, doesn't seem to depend much on density
-h = -0.01 #sign?
+h = -0.01 
 N = 999
 acc = 0.001
 
@@ -35,51 +28,46 @@ u0 = uAsym(r0)
 r1 = r0 + h
 u1 = uAsym(r1)
 
-def getVs(uFunc):    #better name?
+def getVHFunc(rs,us):
+    uFunc = lambda r: us[np.abs(rs-r).argmin()]
+
     Us = np.empty(N)
-    Rs = np.empty(N) #use numpy arrays
+    Rs = np.empty(N)
     Us[0] = 0
     Rs[0] = 0
     Us[1] = 0.01
     Rs[1] = 0.01
-    #should clean up here
-    for i in range(2,N): #numerov method is not applicable here
-        Us[i] = iterateVerlet(Us[i-1],Us[i-2],0.01,Rs[i-1],lambda U,r: -uFunc(r)**2/r if r !=0 else 0)
+
+    for i in range(2,N):
+        Us[i] = iterateVerlet(Us[i-1],Us[i-2],0.01,Rs[i-1],lambda U,r: -(uFunc(r)**2)/r)
         Rs[i] = Rs[i-1] + 0.01
 
-    #print(-1)
-    #print(Us[-1])
-    Us =  Us/abs(Us[-1]) #negative solutions here, problem?
+    #there seems to be a negative problem here
+
+    Us =  Us/Us[-1] #negative solutions here, problem?
     Vs = np.array(list(map(lambda r,U: U/r if r != 0 else 0,Rs,Us)))
 
-    return Rs,Vs
+    return lambda r: Vs[np.abs(Rs-r).argmin()]
 
-def getVsFunc(rs,Vs):
-    return lambda r: Vs[np.abs(rs-r).argmin()]
 
-def getuFunc(rs,us):
-    return lambda r: us[np.abs(rs-r).argmin()]
 
 
 def findEigenstate(rs,us,EPrev):
-    ERange = -np.arange(0.5,2,0.01)
-    Rs,Vs = getVs(getuFunc(rs,us)) #there is a simpler way to structure this
+    print(EPrev)
+    ERange = -np.arange(0.5,3,0.01)
+    VsFunc = getVHFunc(rs,us) #may be able to hold in recursion
     
     def findU0(E):
-        rs0,us0 = runNumerov(r0,h,u0,u1,fKS(E,getVsFunc(Rs,Vs)),N-2) #better names
-        if np.any(us0<0):
+        rs0,us0 = runNumerov(r0,h,u0,u1,fKS(E,VsFunc),N-2) #better names
+        if np.any(us0<0): #removes unphysical solutions
             return 10**10
         return us0[-1]
-    epsilon = min(ERange,key=findU0) #rename
+    epsilon = min(ERange,key=findU0) 
 
-    rsNew,usNew = runNumerov(r0,h,u0,u1,fKS(epsilon,getVsFunc(Rs,Vs)),N-2) # should move to specifying with index?
+    rsNew,usNew = runNumerov(r0,h,u0,u1,fKS(epsilon,VsFunc),N-2) # should move to specifying with index?
     usNewNorm = usNew/np.sqrt(np.sum(abs(h)*usNew**2))
-    RsNew,VsNew = getVs(getuFunc(rsNew,usNewNorm))
-    ENew = 2*epsilon - np.sum(abs(h)*VsNew*usNewNorm**2)
-    #print("-----")
-    #print(epsilon)
-    #print(np.sum(abs(h)*VsNew*usNewNorm**2))
-
+    VsNewFunc = getVHFunc(rsNew,usNew)
+    ENew = 2*epsilon - np.sum(abs(h)*VsNewFunc(rsNew)*(usNew)**2) 
 
 
     if abs(ENew - EPrev) < acc:
@@ -88,15 +76,6 @@ def findEigenstate(rs,us,EPrev):
         return findEigenstate(rsNew,usNewNorm,ENew)
 
 
-ERange = -np.arange(0.5,3,0.01)    
+rs,us = runNumerov(r0,h,u0,u1,fKS(-1,lambda r: 1),N-2)
 
-def findU0(E):
-    rs0,us0 = runNumerov(r0,h,u0,u1,fKS(E,lambda r: 0),N-2)
-    if np.any(us0[-1]<0):
-        return 10**10
-    return us0[-1] #geting some negative values
-
-epsilon = min(ERange,key=findU0)
-print(epsilon)
-rs,us = runNumerov(r0,h,u0,u1,fKS(epsilon,lambda r: 0),N-2)
-print(findEigenstate(rs,us/np.sqrt(np.sum(abs(h)*us**2)),epsilon))
+print(findEigenstate(rs,us/np.sqrt(np.sum(abs(h)*us**2)),-2.5))
