61cbfcc393656f4dd7dab3e2463664b1cd8372d7
Williame33445 <williame15254@gmail.com>
2024-06-27 12:11:46 +0100

    continued work, the problem must be in getVs

--- electronic_structure/density-functional-theory/helium-atom.py
+++ electronic_structure/density-functional-theory/helium-atom.py
@@ -7,30 +7,41 @@ sys.path.append(os.path.abspath("."))
 from useful_code.numerov_method import *
 from useful_code.verlet_method import *
 
-def fKS(E,hatreePot):
-    """
-    f function for KS eq
-    """
-    return lambda r: -2*(E + 2/r - hatreePot(r))  
+secMin = 0.0001 
+def iterateSecant(x_0,x_1,f):
+    x_2 = (x_1*f(x_0) -x_0*f(x_1))/(f(x_0) - f(x_1))
+    if abs(f(x_2)) < secMin:
+        return x_2
+    elif f(x_2) > 0:
+        return iterateSecant(x_0,x_2,f)
+    else:
+        return iterateSecant(x_2,x_1,f)
+
+def getf(Vs): 
+    def f(E):
+        rs0,us0 = run(E,Vs)
+        return us0[-1] # negative solutions problem?
+    return f
+
+
 
 def uAsym(r):
     """
     Finds asymptotic solution
     """
-    return np.sqrt(32)*r*np.exp(-2*r)
+    return r*np.exp(-r)
 
 h = -0.01 
-N = 999
+N = 500
 acc = 0.001
 
-r0 = 10
+r0 = 5 + h
 u0 = uAsym(r0)
 r1 = r0 + h
 u1 = uAsym(r1)
 
-def getVHFunc(rs,us):
-    uFunc = lambda r: us[np.abs(rs-r).argmin()]
-
+def getVs(rs,us):
+    us = np.flip(us)
     Us = np.empty(N)
     Rs = np.empty(N)
     Us[0] = 0
@@ -39,46 +50,51 @@ def getVHFunc(rs,us):
     Rs[1] = 0.01
 
     for i in range(2,N):
-        Us[i] = iterateVerlet(Us[i-1],Us[i-2],0.01,Rs[i-1],lambda U,r: -(uFunc(r)**2)/r)
+        Us[i] = 2*Us[i-1] - Us[i-2] - ((us[i-1]**2)/Rs[i-1])*(0.01**2)
         Rs[i] = Rs[i-1] + 0.01
 
-    if np.any(Us<0):
-        return lambda r: 0
+    Vs = np.array(list(map(lambda r,U: U/r if r != 0 else 0,Rs,Us))) #np way of doing this?
 
-    Us =  Us/Us[-1] #negative solutions here, problem?
-    Vs = np.array(list(map(lambda r,U: U/r if r != 0 else 0,Rs,Us)))
+    return np.flip(Vs)
 
-    return lambda r: Vs[np.abs(Rs-r).argmin()]
+def run(E,Vs):
+    us = np.empty(N)
+    rs = np.empty(N)
+    us[0] = u0
+    rs[0] = r0
+    us[1] = u1
+    rs[1] = r1
 
+    for i in range(2,N):
+        us[i] = 2*us[i-1] - us[i-2] - 2*(E + 2/rs[i-1] - Vs[i-1])*us[i-1]*(h**2)
+        rs[i] = rs[i-1] + h
+    usNorm = us/np.sqrt(np.sum(abs(h)*us**2))
+    return rs, usNorm
 
 
 
 def findEigenstate(rs,us,EPrev):
     print(EPrev)
-    ERange = -np.arange(0.5,3,0.01)
-    VsFunc = getVHFunc(rs,us) #may be able to hold in recursion
+    Vs = getVs(rs,us)
     
-    def findU0(E):
-        rs0,us0 = runNumerov(r0,h,u0,u1,fKS(E,VsFunc),N-2) #better names
-        if np.any(us0<0): #removes unphysical solutions
-            return 10**10
-        return us0[-1]
-    epsilon = min(ERange,key=findU0) 
+    epsilon = iterateSecant(-2,-1,getf(Vs)) #can I write this in a neater way, make range guessed variables
 
-    rsNew,usNew = runNumerov(r0,h,u0,u1,fKS(epsilon,VsFunc),N-2) # should move to specifying with index?
-    usNewNorm = usNew/np.sqrt(np.sum(abs(h)*usNew**2))
-    VsNewFunc = getVHFunc(rsNew,usNew)
-    ENew = 2*epsilon - np.sum(abs(h)*VsNewFunc(rsNew)*(usNew)**2) 
+    rsNew,usNew = run(epsilon,Vs) # should move to specifying with index?
+    VsNew = getVs(rsNew,usNew)
+    ENew = 2*epsilon - np.sum(abs(h)*VsNew*(usNew**2))
 
 
     if abs(ENew - EPrev) < acc:
         return ENew
-    else:
-        return findEigenstate(rsNew,usNewNorm,ENew)
+    return findEigenstate(rsNew,usNew,ENew)
 
 
-rs,us = runNumerov(r0,h,u0,u1,fKS(-1,lambda r: 1),N-2)
+rs,us = run(-2,np.zeros(N))
 
-print(findEigenstate(rs,us/np.sqrt(np.sum(abs(h)*us**2)),-2.5))
+print(findEigenstate(rs,us,-2))
 
-#should look at the hydrogen case and remove the negatives, my trick with 10**10 was ad hoc
+
+
+
+
+
