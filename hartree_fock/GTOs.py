4551547b8e4c05f7d80abf33ee93b404058214d5
Williame33445 <williame15254@gmail.com>
2024-09-23 17:57:45 +0100

    added way to get wavefunction from GTOs and function to get the density plot

--- hartree_fock/GTOs.py
+++ hartree_fock/GTOs.py
@@ -12,14 +12,44 @@ class GTOPrimative:
         self.center = center
         self.type = type
     
+    def wavefunction(self,xv,yv,zv):
+        Rx = self.center[0]
+        Ry = self.center[1]
+        Rz = self.center[2]
+        i = self.type[0]
+        j = self.type[1]
+        k = self.type[2]
+        return (xv - Rx)**i*(yv - Ry)**j*(zv - Rz)**k*np.exp(-self.alpha*((xv - Rx)**2 + (yv - Ry)**2 + (zv - Rz)**2))
+    
 class GTOContraction:
     """
     Class that holds the contracted GTOs.
     """
     def __init__(self,primitives,ds):
         self.primitives = primitives 
-        self.ds = np.array(ds)
+        self.ds = ds
+    
+    def wavefunction(self,xv,yv,zv):
+        w = np.zeros(xv.shape)
+        for i,d in enumerate(self.ds):
+            w += d*self.primitives[i].wavefunction(xv,yv,zv)
+        return w
         
+def getDensity(xv,yv,zv,electrons,GTOs):
+    GTOWaveFunc = []
+    for GTO in GTOs:
+        GTOWaveFunc.append(GTO.wavefunction(xv,yv,zv))
+
+    ns = np.zeros(xv.shape)
+    for electron in electrons:
+        ne = np.zeros(xv.shape)
+        for i,d in enumerate(electron.ds):
+            ne += d*GTOWaveFunc[i]
+        ne *= ne
+        ns += ne
+     
+    return ns
+
 def getType(l):
     """
     Finds all GTO types for a given l.
