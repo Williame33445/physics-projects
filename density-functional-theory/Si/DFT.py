a070dee632188e475049c3d9d472dee15b772f48
William <williame15254@gmail.com>
2025-06-11 15:11:12 +0100

    completed density part

--- density-functional-theory/Si/DFT.py
+++ density-functional-theory/Si/DFT.py
@@ -8,37 +8,32 @@ def u(r,q):
     return (2*r - q - 1)/(2*q)
 
 class DFTSimulation:
-    def __init__(self, ax, ay, az, KxMax, KyMax, KzMax, kxSamp, kySamp, kzSamp, atoms, occuptationFunction):
+    def __init__(self, ax, ay, az, KIxMax, KIyMax, KIzMax, kxSamp, kySamp, kzSamp, atoms, occuptationFunction):
         self.atoms = atoms
         self.occuptationFunction = occuptationFunction
+        self.volume = ax*ay*az
 
-        self.kxMax = KxMax
-        self.kyMax = KyMax
-        self.kzMax = KzMax
+        KIxVals = np.arange(-KIxMax, KIxMax + 1)
+        KIyVals = np.arange(-KIyMax, KIyMax + 1)
+        KIzVals = np.arange(-KIzMax, KIzMax + 1)
 
+        KIxGrid, KIyGrid, KIzGrid = np.meshgrid(KIxVals, KIyVals, KIzVals, indexing="ij")
+        self.KIs = np.stack((KIxGrid, KIyGrid, KIzGrid), axis=-1).reshape(-1, 3)
+        self.Ks = 2*np.pi*np.einsum("ij,kj",np.diag([1/ax, 1/ay, 1/az]), self.KIs).T
 
-        KxVals = np.arange(-KxMax, KxMax + 1)
-        KyVals = np.arange(-KyMax, KyMax + 1)
-        KzVals = np.arange(-KzMax, KzMax + 1)
+        self.getKsIndex = lambda K: (K[0] + KIxMax)*(2*KIxMax + 1)*(2*KIyMax + 1) + (K[1] + KIyMax)*(2*KIxMax + 1) + K[2] + KIzMax
+        self.Ks0Index = self.getKsIndex(np.array([0, 0, 0]))
 
-        self.getKIndex = lambda K: K[0]*(2*KxMax + 1)*(2*KyMax + 1) + K[1]*(2*KxMax + 1) + K[2] + (KzMax + KzMax + 1)
-
-
-        KxGrid, KyGrid, KzGrid = np.meshgrid(KxVals, KyVals, KzVals, indexing="xy")
-        self.Ks = np.stack((KxGrid, KyGrid, KzGrid), axis=-1).reshape(-1, 3)
 
         self.getAtomicPotential()
 
-        self.bx = 2*np.pi*np.array([1/ax,0,0])
-        self.by = 2*np.pi*np.array([0,1/ay,0])
-        self.bz = 2*np.pi*np.array([0,0,1/az])
+        rxVals = np.arange(1, kxSamp + 1)
+        ryVals = np.arange(1, kySamp + 1)
+        rzVals = np.arange(1, kzSamp + 1)
 
-        kxVals = np.arange(1, kxSamp + 1)
-        kyVals = np.arange(1, kySamp + 1)
-        kzVals = np.arange(1, kzSamp + 1)
-
-        kxGrid, kyGrid, kzGrid = np.meshgrid(kxVals, kyVals, kzVals, indexing="xy")
-        self.BZSample = 2*np.pi*np.stack((u(kxGrid,kxSamp)/ax, u(kyGrid,kySamp)/ay, u(kzGrid,kzSamp)/az), axis=-1).reshape(-1, 3)
+        kxGrid, kyGrid, kzGrid = np.meshgrid(rxVals, ryVals, rzVals, indexing="ij")
+        self.BZSamples = 2*np.pi*np.stack((u(kxGrid,kxSamp)/ax, u(kyGrid,kySamp)/ay, u(kzGrid,kzSamp)/az), axis=-1).reshape(-1, 3)
+        
         
 
     
@@ -48,6 +43,7 @@ class DFTSimulation:
         for i in range(len(self.Ks)):
             for j in range(i+1):
                 for a in self.atoms:
+
                     self.V[i, j] += np.exp(-1j*np.dot(self.Ks[i] - self.Ks[j],a.position))*a.pseudopotential(self.Ks[i], self.Ks[j])
                 self.V[j, i] = np.conjugate(self.V[i, j])
 
@@ -62,16 +58,17 @@ class DFTSimulation:
 
         return eigenvalues, eigenvectors
     
-    def getDensity(self, K):
+    def getDensity(self, KI):
         density = 0
-        for k_BZ in self.BZSample:
+        KIndexRel0 = self.getKsIndex(KI) - self.Ks0Index 
+        for k_BZ in self.BZSamples:
             eigenvalues, eigenvectors = self.solveForConstantDensity(k_BZ)
             for i,e in enumerate(eigenvalues):
                 occ = self.occuptationFunction(e)
-                for K_1 in self.Ks:
-                    if np.all(np.abs(K + K_1) > self.KxMax + self.KyMax*self.by + self.kzMax*self.bz):
-                        density += occ * np.conj(eigenvectors[i, self.getKIndex(K + K_1)]) * eigenvectors[i, self.getKIndex(K_1)]
-
+                for KI_1 in self.KIs:
+                    if np.all(np.abs(KI + KI_1) <= self.KIs[-1]): 
+                        K1Index = self.getKsIndex(KI_1)
+                        density += occ * np.conjugate(eigenvectors[i, KIndexRel0 + K1Index]) * eigenvectors[i, K1Index]/(len(self.BZSamples)*self.volume)
         return density
 
 
@@ -80,7 +77,5 @@ class DFTSimulation:
 HPseudoPotential = GTH.pseudopotential(0.2,1,125,-4.0663326,0.6778322,0,0,0,0,0)
 atomList =  [Atom(np.array([0,0,0]),HPseudoPotential)]
 
-eigenvalues, eigenvectors = DFTSimulation(5,5,5,1,1,1,1,1,1,atomList,lambda e: 1).solveForConstantDensity(np.array([0,0,0]))
-print(eigenvalues)
-
-#need to set up Si pseudopotential properly, also look at calculating density via BZ sampling
+density = DFTSimulation(5,5,5,1,1,1,1,1,1,atomList,lambda e: 1 if e < 1 else 0).getDensity(np.array([0,0,0]))
+print(density)
